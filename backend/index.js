357171d03df2d7e30026115c3e8d6f01e25eb245
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2025-02-14 00:08:47 +0530

    Enhance database connection logic with retry mechanism

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,27 @@ import config from "./utils/config.js";
 import app from './app.js'
 import client from "./client.js";
 
-const checkDbConnection = async () => {
+const checkDbConnection = async (retries = 5, delay = 5000) => {
+  for(let attempt = 1; attempt <= retries; attempt++){
     try {
-      await client.$connect();
-      console.log("Database connected!");
+      await client.$connect()
+      console.log('database connected')
+      return
     } catch (error) {
-      console.error("Database connection failed:", error.message);
-      process.exit(1);
+      console.error(`database connection failed (Attempt ${attempt} / ${retries}}): `, error.message)
+      if (attempt < retries) {
+        console.log(`Retrying in ${delay / 1000} seconds...`);
+        await new Promise((res) => setTimeout(res, delay));
+    } else {
+        console.log("All retry attempts failed. Exiting...");
+        process.exit(1);
+      }
     }
-}
-  
-checkDbConnection();
+  }}
 
-app.listen(config.PORT, () => {
-    console.log(`Server running on PORT ${config.PORT}.`)
+
+checkDbConnection().then(() => {
+    app.listen(config.PORT, () => {
+        console.log(`Server running on PORT ${config.PORT}`);
+    });
 })
